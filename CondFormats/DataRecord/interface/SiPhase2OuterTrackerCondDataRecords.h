e01b6cb2d9413e404573c515f24ba3d3eeff4f07
mmusich <marco.musich@cern.ch>
2020-05-19 12:50:48 +0200

    move Phase-2 Tracker records into their own files

--- CondFormats/DataRecord/interface/SiPhase2OuterTrackerCondDataRecords.h
+++ CondFormats/DataRecord/interface/SiPhase2OuterTrackerCondDataRecords.h
@@ -3,12 +3,11 @@
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
 #include "Geometry/Records/interface/TrackerTopologyRcd.h"
-#include "FWCore/Utilities/interface/mplVector.h"
 
 /*Record associated to SiPhase2OuterTrackerLorentzAngle Object: the SimRcd is used in simulation only*/
 class SiPhase2OuterTrackerLorentzAngleRcd
     : public edm::eventsetup::DependentRecordImplementation<SiPhase2OuterTrackerLorentzAngleRcd,
-                                                            edm::mpl::Vector<TrackerTopologyRcd> > {};
+                                                            boost::mpl::vector<TrackerTopologyRcd> > {};
 class SiPhase2OuterTrackerLorentzAngleSimRcd
     : public edm::eventsetup::EventSetupRecordImplementation<SiPhase2OuterTrackerLorentzAngleSimRcd> {};
 
