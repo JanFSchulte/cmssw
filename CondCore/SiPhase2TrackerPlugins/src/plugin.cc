2b7194312801cc2f867453e5bbb0efe89535e361
mmusich <marco.musich@cern.ch>
2020-05-19 12:50:48 +0200

    merge conflicts

--- CondCore/SiPhase2TrackerPlugins/src/plugin.cc
+++ CondCore/SiPhase2TrackerPlugins/src/plugin.cc
@@ -1,5 +1,6 @@
 #include "CondCore/ESSources/interface/registration_macros.h"
 #include "CondFormats/SiPhase2TrackerObjects/interface/TrackerDetToDTCELinkCablingMap.h"
 #include "CondFormats/DataRecord/interface/TrackerDetToDTCELinkCablingMapRcd.h"
+#include "CondFormats/DataRecord/interface/SiPhase2OuterTrackerCondDataRecords.h"
 
 REGISTER_PLUGIN(TrackerDetToDTCELinkCablingMapRcd, TrackerDetToDTCELinkCablingMap);
