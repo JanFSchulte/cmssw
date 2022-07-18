5807e071000f22d026191b32a20494cef25b8145
Pedro Fernandez Manteca <pedro.fernandez.manteca@cern.ch>
2022-07-18 11:43:58 +0200

    Update multicrab.py

--- scripts/multicrab.py
+++ scripts/multicrab.py
@@ -292,7 +292,7 @@ def main():
                     elif '2017' in era:
                         config.Data.lumiMask = LM_prefix + '17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'
                     elif '2016' in era:
-                        config.Data.lumiMask = LM_prefic + '16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'
+                        config.Data.lumiMask = LM_prefix + '16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'
                 else:
                     if '2018' in era:
                         config.Data.lumiMask = LM_prefix + '18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt'
