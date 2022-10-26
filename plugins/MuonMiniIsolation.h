4153ad7c859b6f7860d8a7df11ef9b473d6c4714
Sergio Blanco Fernandez <sergio.blanco.fernandez@cern.ch>
2022-10-26 16:24:41 +0200

    Match nanoAOD miniPFIsolation

--- plugins/MuonMiniIsolation.h
+++ plugins/MuonMiniIsolation.h
@@ -21,22 +21,6 @@
 #include "NtupleContent.h"
 #include "helper.h"
 
-template <typename PFCANDS>
-inline bool is_fromPV(PFCANDS &pfc) {
-  return false;
-}
-
-template <>
-inline bool is_fromPV<reco::PFCandidate>(reco::PFCandidate &pfc) {
-  if (pfc.bestTrack())
-    return (pfc.bestTrack()->dz() < 0.0 ? true : false);
-  return false;
-}
-
-template <>
-inline bool is_fromPV<pat::PackedCandidate>(pat::PackedCandidate &pfc) {
-  return (pfc.fromPV() > 1 ? true : false);
-}
 
 inline float miniIsoDr(const reco::Candidate::PolarLorentzVector &p4, float mindr, float maxdr, float kt_scale) {
   return std::max(mindr, std::min(maxdr, float(kt_scale / p4.pt())));
@@ -44,7 +28,7 @@ inline float miniIsoDr(const reco::Candidate::PolarLorentzVector &p4, float mind
 
 // Adapted from PhysicsTools/PatUtils/src/MiniIsolation.cc
 template <typename PFCANDS>
-pat::PFIsolation getMiniPFIsolation(const std::vector<PFCANDS> &pfcands,
+pat::PFIsolation getMiniPFIsolation(const pat::PackedCandidateCollection *pfcands,
                                     const reco::Candidate::PolarLorentzVector &p4,
                                     float mindr = 0.05,
                                     float maxdr = 0.2,
@@ -55,8 +39,10 @@ pat::PFIsolation getMiniPFIsolation(const std::vector<PFCANDS> &pfcands,
                                     float deadcone_ph = 0.01,
                                     float deadcone_nh = 0.01,
                                     float dZ_cut = 0.0) {
+
   float chiso = 0, nhiso = 0, phiso = 0, puiso = 0;
   float drcut = miniIsoDr(p4, mindr, maxdr, kt_scale);
+
   for (auto const pc : pfcands) {
     float dr2 = deltaR2(p4, pc);
     if (dr2 > drcut * drcut)
@@ -64,8 +50,7 @@ pat::PFIsolation getMiniPFIsolation(const std::vector<PFCANDS> &pfcands,
     float pt = pc.p4().pt();
     int id = pc.pdgId();
     if (std::abs(id) == 211) {
-      // bool fromPV = (pc.fromPV() > 1 || fabs(pc.dz()) < dZ_cut);
-      bool fromPV = is_fromPV(pc);
+      bool fromPV = (pc.fromPV() > 1 || fabs(pc.dz()) < dZ_cut);
       if (fromPV && dr2 > deadcone_ch * deadcone_ch) {
         // if charged hadron and from primary vertex, add to charged hadron isolation
         chiso += pt;
@@ -87,7 +72,12 @@ pat::PFIsolation getMiniPFIsolation(const std::vector<PFCANDS> &pfcands,
 // Adapted from implementation from Daniel Li (Brown)
 template <typename MUON, typename PFCANDS>
 inline void FillMiniIso(
-    const std::vector<PFCANDS> &pfcands, const MUON &mu, const double rho, NtupleContent &nt, bool isTag) {
+    const pat::PackedCandidateCollection *pfcands, 
+    const MUON &mu, 
+    const double rho, 
+    NtupleContent &nt, 
+    bool isTag) {
+
   double Aeff_Fall17[5] = {0.0566, 0.0562, 0.0363, 0.0119, 0.0064};
   double EA;
 
@@ -125,5 +115,49 @@ inline void FillMiniIso(
     nt.probe_miniIsoNeutrals = neu / mu.pt();
   }
 }
+
+
+// miniAOD implementation
+template <typename MUON>
+inline void FillMiniIsov2(
+  const MUON &mu,
+  const double rho,
+  NtupleContent &nt,
+  bool isTag  
+) {
+  
+  double Aeff_Fall17[5] = { 0.0566, 0.0562, 0.0363, 0.0119, 0.0064 };
+  double EA;
+
+  auto iso = mu.miniPFIsolation();
+  auto chg = iso.chargedHadronIso();
+  auto neu = iso.neutralHadronIso();
+  auto pho = iso.photonIso();
+    
+  if( TMath::Abs(mu.eta()) < 0.8 ) EA = Aeff_Fall17[0];
+  else if( TMath::Abs(mu.eta()) < 1.3 ) EA = Aeff_Fall17[1];
+  else if( TMath::Abs(mu.eta()) < 2.0 ) EA = Aeff_Fall17[2];
+  else if( TMath::Abs(mu.eta()) < 2.2 ) EA = Aeff_Fall17[3];
+  else EA = Aeff_Fall17[4];
+
+  float R = 10.0 / std::min( std::max( mu.pt(), 50.0 ), 200.0 );
+  EA *= std::pow( R / 0.3, 2 );
+    
+  float miniIso = ( chg + TMath::Max( 0.0, neu + pho - (rho) * EA ) ) / mu.pt();
+
+  if( isTag ){
+    nt.tag_miniIso = miniIso;
+    nt.tag_miniIsoCharged = chg / mu.pt();
+    nt.tag_miniIsoPhotons = pho / mu.pt();
+    nt.tag_miniIsoNeutrals = neu / mu.pt();
+  }
+  else {
+    nt.probe_miniIso = miniIso;
+    nt.probe_miniIsoCharged = chg / mu.pt();
+    nt.probe_miniIsoPhotons = pho / mu.pt();
+    nt.probe_miniIsoNeutrals = neu / mu.pt();
+  }
+}
+
 
 #endif
