314ac8fa8a3af2ebce4bedad44e60c35a84b9cc5
Sergio Blanco Fernandez <sergio.blanco.fernandez@cern.ch>
2023-09-19 16:37:54 +0200

    Restructure KlFitter

--- plugins/KlFitter.cc
+++ plugins/KlFitter.cc
@@ -17,33 +17,33 @@ KlFitter::~KlFitter(){};
 void KlFitter::fillNtuple(NtupleContent &nt, bool isTuneP) {
   if (isTuneP) {
     if (status_) {
-      nt.pair_tuneP_svprob = prob_;
-      nt.pair_tuneP_fit_mass = DimuonMass(refited[0].track().pt(),
+      nt.branches["pair_tuneP_svprob"] = (float)prob_;
+      nt.branches["pair_tuneP_fit_mass"] = (float)DimuonMass(refited[0].track().pt(),
                                           refited[0].track().eta(),
                                           refited[0].track().phi(),
                                           refited[1].track().pt(),
                                           refited[1].track().eta(),
                                           refited[1].track().phi());
-      nt.pair_tuneP_normalchi2 = normalchi2_;
+      nt.branches["pair_tuneP_normalchi2"] = (float)normalchi2_;
     } else {
-      nt.pair_tuneP_svprob = -1;
-      nt.pair_tuneP_fit_mass = -1;
-      nt.pair_tuneP_normalchi2 = -1;
+      nt.branches["pair_tuneP_svprob"] = (float)-1;
+      nt.branches["pair_tuneP_fit_mass"] = (float)-1;
+      nt.branches["pair_tuneP_normalchi2"] = (float)-1;
     }
   } else {
     if (status_) {
-      nt.pair_svprob = prob_;
-      nt.pair_fit_mass = DimuonMass(refited[0].track().pt(),
+      nt.branches["pair_svprob"] = (float)prob_;
+      nt.branches["pair_fit_mass"] = (float)DimuonMass(refited[0].track().pt(),
                                     refited[0].track().eta(),
                                     refited[0].track().phi(),
                                     refited[1].track().pt(),
                                     refited[1].track().eta(),
                                     refited[1].track().phi());
-      nt.pair_normalchi2 = normalchi2_;
+      nt.branches["pair_normalchi2"] = (float)normalchi2_;
     } else {
-      nt.pair_svprob = -1;
-      nt.pair_fit_mass = -1;
-      nt.pair_normalchi2 = -1;
+      nt.branches["pair_svprob"] = (float)-1;
+      nt.branches["pair_fit_mass"] = (float)-1;
+      nt.branches["pair_normalchi2"] = (float)-1;
     }
   }
 }
